2a542c9b7ceb7c852a04b2be4cf0b5e732eb0f04
riccardofelluga <riki.1@hotmail.it>
2021-10-11 16:21:28 +0300

    Initial setup and boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+export default {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended'
+	],
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 13,
+		'sourceType': 'module'
+	},
+	'rules': {
+		'semi':['error', 'never'],
+		'no-var': 'error',
+		'quotes': ['error', 'single'],
+		'object-curly-spacing': ['error', 'always'],
+		'array-bracket-spacing': ['error', 'always'],
+		'space-in-brackets': ['error', 'always'],
+		'sort-imports': [2, {
+			'ignoreCase': true,
+			'ignoreMemberSort': true,
+			'memberSyntaxSortOrder': ['none', 'all', 'multiple', 'single']
+		}],
+		'indent': ['error', 'tab']
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # WebGL-Animations
+
+Project for the WWW Application [CS-E4460]
+
+## Quick start
+
+`npm install`
+`npm start`
