ca54e7b21c43045348a44074960612b3b385270d
riccardofelluga <riki.1@hotmail.it>
2021-10-11 19:28:29 +0300

    added webgl boilerplate and css

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log('Hello world!')
+function main() {
+    const canvas = document.querySelector('#gl-context')
+    const gl = canvas.getContext('webgl')
+
+    if (gl=== null){
+        console.error('Unable to run WebGL on this browser.')
+        return
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+}
+
+window.onload = main
