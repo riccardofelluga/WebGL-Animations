6e6e35c67e839cefca5d6061e4c233dff17c01e4
riccardofelluga <riki.1@hotmail.it>
2021-10-12 14:54:20 +0300

    switched to typescript

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+
+function main() {
+	const canvas = <HTMLCanvasElement>document.querySelector('#gl-context')
+	const gl = canvas.getContext('webgl')
+
+	if (gl === null) {
+		console.error('Unable to run WebGL on this browser.')
+		return
+	}
+
+	gl.clearColor(0.0, 0.0, 0.0, 1.0)
+	gl.clear(gl.COLOR_BUFFER_BIT)
+}
+
+window.onload = main
