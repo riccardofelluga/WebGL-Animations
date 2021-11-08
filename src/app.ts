bc3ed99b5769987954060cb2728ea1717da15c04
Unni Kohonen <unni.kohonen@aalto.fi>
2021-11-08 16:30:17 +0200

    parser without indexes

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,9 @@ import vertSrc from './shaders/simple_vertex.glsl'
 
 function parseOBJ(text: string) {
 
+  const tmpV = []
+  const tmpN = []
+
   const vertexData = []
   const normalData = []
   const vertexIdx = []
@@ -13,13 +16,53 @@ function parseOBJ(text: string) {
 
   const keywords = {
     v: (args: Array<string>) => {
-      args.forEach(a => vertexData.push(parseFloat(a)))
+      tmpV.push(args.map(a => parseFloat(a)))
+      //args.forEach(a => vertexData.push(parseFloat(a)))
+      //args.forEach(a => tmpV.push(parseFloat(a)))
+      console.log(tmpV)
     },
     vn: (args: Array<string>) => {
-      args.forEach(a => normalData.push(parseFloat(a)))
+      //args.forEach(a => normalData.push(parseFloat(a)))
+      tmpN.push(args.map(a => parseFloat(a)))
+      //args.forEach(a => tmpN.push(parseFloat(a)))
     },
     f: (args: Array<string>) => {
       for (let i = 1; i < args.length - 1; i++) {
+        //console.log('aa',tmpV[parseInt(args[0].split('/')[1]) - 1])
+        tmpV[parseInt(args[0].split('/')[0]) - 1].forEach(a => vertexData.push(parseFloat(a)))
+        tmpV[parseInt(args[i].split('/')[0]) - 1].forEach(a => vertexData.push(parseFloat(a)))
+        tmpV[parseInt(args[i+1].split('/')[0]) - 1].forEach(a => vertexData.push(parseFloat(a)))
+
+        tmpN[parseInt(args[0].split('/')[2]) - 1].forEach(a => normalData.push(parseFloat(a)))
+        tmpN[parseInt(args[i].split('/')[2]) - 1].forEach(a => normalData.push(parseFloat(a)))
+        tmpN[parseInt(args[i+1].split('/')[2]) - 1].forEach(a => normalData.push(parseFloat(a)))
+
+
+
+        /*
+          console.log(parseInt(args[0].split('/')[0]) - 1)
+          console.log(parseInt(args[i].split('/')[0]) - 1)
+          console.log(parseInt(args[i+1].split('/')[0]) - 1)
+
+          vertexData.push(parseFloat(tmpV[parseInt(args[0].split('/')[0]) - 1]))
+          vertexData.push(parseFloat(tmpV[parseInt(args[0].split('/')[0]) - 1 + 1]))
+          vertexData.push(parseFloat(tmpV[parseInt(args[0].split('/')[0]) - 1 + 2]))
+
+          vertexData.push(parseFloat(tmpV[parseInt(args[i].split('/')[0]) - 1]))
+          vertexData.push(parseFloat(tmpV[parseInt(args[i].split('/')[0]) - 1 + 1]))
+          vertexData.push(parseFloat(tmpV[parseInt(args[i].split('/')[0]) - 1 + 2]))
+
+          vertexData.push(parseFloat(tmpV[parseInt(args[i+1].split('/')[0]) - 1]))
+          vertexData.push(parseFloat(tmpV[parseInt(args[i+1].split('/')[0]) - 1 + 1]))
+          vertexData.push(parseFloat(tmpV[parseInt(args[i+1].split('/')[0]) - 1 + 2]))
+
+          */
+        //normalData.push(parseFloat(tmpN[parseInt(args[0].split('/')[2]) - 1]))
+        //normalData.push(parseFloat(tmpN[parseInt(args[i].split('/')[2]) - 1]))
+        //normalData.push(parseFloat(tmpN[parseInt(args[i+1].split('/')[2]) - 1]))
+
+        
+        /*
         vertexIdx.push(parseFloat(args[0].split('/')[0]) - 1)
         vertexIdx.push(parseFloat(args[i].split('/')[0]) - 1)
         vertexIdx.push(parseFloat(args[i+1].split('/')[0]) - 1)
@@ -27,6 +70,7 @@ function parseOBJ(text: string) {
         normalIdx.push(parseFloat(args[0].split('/')[2]) - 1)
         normalIdx.push(parseFloat(args[i].split('/')[2]) - 1)
         normalIdx.push(parseFloat(args[i+1].split('/')[2]) - 1)
+        */
       }
     }
   }
@@ -61,6 +105,17 @@ function parseOBJ(text: string) {
 }
 
 const OBJtext = `
+v 1 1 0
+v 1 0.5 0
+v 0.5 0.5 0
+v 0.5 1 0
+
+vn 1 1 0
+
+f 1//1 2//1 3//1 4//1
+`
+
+/*`
 v 1.000000 1.000000 -1.000000
 v 1.000000 -1.000000 -1.000000
 v 1.000000 1.000000 1.000000
@@ -83,7 +138,7 @@ f 8/8/3 7/9/3 5/10/3 6/11/3
 f 6/12/4 2/13/4 4/5/4 8/14/4
 f 2/13/5 1/1/5 3/4/5 4/5/5
 f 6/11/6 5/10/6 1/1/6 2/13/6
-`
+`*/
 
 function main() {
   const canvas = <HTMLCanvasElement>document.querySelector('#gl-context')
@@ -97,6 +152,7 @@ function main() {
   const camera = new Camera([ 2.0, 3.0, 4.0 ], [ 0.0, 0.0, 0.0 ], [ 0.0, 1.0, 0.0 ])
 
   const obj = parseOBJ(OBJtext)
+  console.log(obj)
   const cube = new Object(gl, obj, vertSrc, fragSrc)
   cube.setColor([ 0.3, 0.2, 0.7, 1.0 ])
 
